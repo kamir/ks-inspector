2d2939a713ec5f03c3fa52c79632ce9d8d61a740
Mirko Kämpf <mirko@confluent.io>
2020-11-17 12:01:11 +0100

    added a faster build process using a local JAR file

--- bin/run-docker.sh
+++ bin/run-docker.sh
@@ -1,3 +1,5 @@
+docker build . -t tsi_test:latest
+
 docker run \
 -e KST_BOOTSTRAP_SERVER=pkc-4yyd6.us-east1.gcp.confluent.cloud:9092 \
 -e KST_SSL_ENDPOINT_IDENTIFICATION_ALGORITHM=https \
@@ -7,4 +9,4 @@ docker run \
 -e KST_SCHEMA_REGISTRY_URL=https://psrc-4v1qj.eu-central-1.aws.confluent.cloud \
 -e KST_SCHEMA_REGISTRY_BASIC_AUTH_CREDENTIALS_SOURCE=USER_INFO \
 -e KST_SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO=2NOYXIN4EXJDZYTR:ToV9mOn81vDnx/hLxIZK3hl3mstQtjPR1ZdoelVCwGJPFWORQW552+WeJh4Il7b4 \
-kst_test:latest
+tsi_test:latest
