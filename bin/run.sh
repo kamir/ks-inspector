929fec9e6764cb1a76dad3b91cfa154fd09af03a
Mirko Kämpf <mirko@confluent.io>
2020-11-17 15:17:06 +0100

    added query function for execution of predefined graph queries.

--- bin/run.sh
+++ bin/run.sh
@@ -10,14 +10,46 @@ export KST_SASL_JAAS_CONFIG='org.apache.kafka.common.security.plain.PlainLoginMo
 export KST_SCHEMA_REGISTRY_URL=https://psrc-4v1qj.eu-central-1.aws.confluent.cloud
 export KST_SCHEMA_REGISTRY_BASIC_AUTH_CREDENTIALS_SOURCE=USER_INFO
 export KST_SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO=2NOYXIN4EXJDZYTR:ToV9mOn81vDnx/hLxIZK3hl3mstQtjPR1ZdoelVCwGJPFWORQW552+WeJh4Il7b4
+
 export KST_NEO4J_DATABASE_DIRECTORY="graph-db";
 export KST_NEO4J_DEFAULT_DATABASE_NAME="neo4j";
 export KST_NEO4J_URI="bolt://localhost:7687";
 export KST_NEO4J_USERNAME="neo4j";
 export KST_NEO4J_PASSWORD="admin";
 
-# java -jar target/ks-inspector-1.0-SNAPSHOT.jar inspect -b pkc-4yyd6.us-east1.gcp.confluent.cloud:9092 -e KST src/main/cluster-state-tools-data/contexts/example1/instances
+########################################################################################################################
+# Show the commands which are available in the tool.
+########################################################################################################################
+#
+# java -jar target/ks-inspector-1.0-SNAPSHOT.jar
 
-java -jar target/ks-inspector-1.0-SNAPSHOT.jar export2Neo4J -b pkc-4yyd6.us-east1.gcp.confluent.cloud:9092 -e KST
+########################################################################################################################
+# Inspect the domains defined in the DOM files in folder:
+########################################################################################################################
+#
+#   src/main/cluster-state-tools-data/contexts/example1/instances
+#
+# java -jar target/ks-inspector-1.0-SNAPSHOT.jar inspect  \
+#      -b pkc-4yyd6.us-east1.gcp.confluent.cloud:9092  \
+#      -e KST src/main/cluster-state-tools-data/contexts/example1/instances
 
-# java -jar target/ks-inspector-1.0-SNAPSHOT.jar clearGraph -e KST
+
+########################################################################################################################
+# Export the graph data which is persisted in the Kafka topic named _kst_knowledgegraph.
+########################################################################################################################
+#
+# java -jar target/ks-inspector-1.0-SNAPSHOT.jar export2Neo4J  \
+#      -b pkc-4yyd6.us-east1.gcp.confluent.cloud:9092  \
+#      -e KST
+
+########################################################################################################################
+# Clear the graph in NEO4J.
+#
+# java -jar target/ks-inspector-1.0-SNAPSHOT.jar clearGraph
+#      -e KST
+
+########################################################################################################################
+# Query the knowledge graph with a standard query, useful for operators.
+#
+java -jar target/ks-inspector-1.0-SNAPSHOT.jar queryGraph  \
+     -e KST src/main/cypher/q1.cypher
