3f37f0b821a3ca3480dc7f508e6857c4df488960
Mirko Kämpf <mirko@confluent.io>
2020-12-07 15:14:18 +0100

    Continue with Pipeline Integration.

--- bin/run-00-kst.sh
+++ bin/run-00-kst.sh
@@ -4,6 +4,10 @@
 source setenv.sh
 
 cd ..
+mvn clean compile package
 
-java -cp target/ks-inspector-1.0-SNAPSHOT.jar io.confluent.cp.util.KSTWrapper
+#java -cp target/ks-inspector-1.0-SNAPSHOT.jar io.confluent.cp.util.KSTWrapper ./src/main/cluster-state-tools-data/example2/ccloud-environments.yaml
+
+java -cp target/ks-inspector-1.0-SNAPSHOT.jar io.confluent.cp.util.KSTWrapper ./src/main/cluster-state-tools-data/example2/ccloud-environments.yaml Incentives
+
 echo
--- src/main/java/io/confluent/cp/util/KSTWrapper.java
+++ src/main/java/io/confluent/cp/util/KSTWrapper.java
@@ -1,8 +1,10 @@
 package io.confluent.cp.util;
 
+import io.confluent.kafka.serializers.subject.strategy.SubjectNameStrategy;
 import net.christophschubert.kafka.clusterstate.cli.CloudGovernance;
 import net.christophschubert.kafka.clusterstate.cli.EnvVarTools;
 import net.christophschubert.kafka.clusterstate.cli.PropertyFileBuilder;
+import org.checkerframework.checker.units.qual.A;
 
 /**
  * This is the programm, which will be executed by the pipeline.
@@ -11,16 +13,43 @@ public class KSTWrapper {
 
     public static void main(String[] ARGS) {
 
+
+
         String targetClusterName = EnvVarTools.readPropertyFromEnv("KST", "TARGET_CLUSTER_NAME");
+        //String targetDomainName = EnvVarTools.readPropertyFromEnv("KST", "TARGET_DOMAIN_NAME");
 
-        System.out.println( "> Apply all changes needed to bring the cluster: ["+targetClusterName+"] into the desired state.");
+        System.out.println( ARGS );
 
-        // How to call KST to make my changes?
-        // String[] args1 = { "applyToSelectedCluster", "./src/main/cluster-state-tools-data/example2/ccloud-environments.yaml", targetClusterName };
 
-        String[] args1 = { "applyToSelectedCluster",  ARGS[0] , targetClusterName };
+        if ( ARGS == null || ARGS.length == 0 ) {
 
-        CloudGovernance.main( args1 );
+            System.out.println( "[1] > Apply all changes needed to bring the cluster: ["+targetClusterName+"] into the desired state.");
+            String[] args1 = { "applyToSelectedCluster", "./src/main/cluster-state-tools-data/example2/ccloud-environments.yaml", targetClusterName };
+            CloudGovernance.main( args1 );
+
+        }
+        else if ( ARGS.length == 1 ) {
+            System.out.println( "[2] > Apply all changes needed to bring the cluster: ["+targetClusterName+"] into the desired state.");
+            System.out.println( "    > Process the environment-file: "  +   ARGS[0] );
+
+
+            // How to call KST to make my changes?
+            String[] args2 = { "applyToSelectedCluster",  ARGS[0] , targetClusterName };
+            CloudGovernance.main( args2 );
+
+        }
+        else if ( ARGS.length == 2 ) {
+
+            System.out.println( "[3] > Apply all changes needed to bring the cluster : ["+targetClusterName+"] into the desired state.");
+            System.out.println( "    > Process the environment-file : "  +   ARGS[0] );
+            System.out.println( "    > Select the domain : "  +   ARGS[1] );
+
+
+            // TODO: Add function for selective execution ...
+            String[] args3 = {"applyToSelectedClusterAndDomain", ARGS[0], targetClusterName, ARGS[1]};
+            CloudGovernance.main(args3);
+
+        }
 
     }
 
