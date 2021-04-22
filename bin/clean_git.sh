4a4b1a77c9be5369b0581a919d129d42057523a3
Mirko Kämpf <mirko@confluent.io>
2021-04-22 07:01:16 +0200

    cleanup done

--- bin/clean_git.sh
+++ bin/clean_git.sh
@@ -3,3 +3,4 @@ git add .
 echo "Clean the repo: $1"
 git commit -m "prep clean $1"
 git filter-branch --force --index-filter "git rm --cached --ignore-unmatch $1" --prune-empty --tag-name-filter cat -- --all
+git commit -m "cleanup done $1"
