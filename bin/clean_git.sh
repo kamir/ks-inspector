5ea2cfd279639ee59f5f3c0097e52fc17aee1f7d
Mirko Kämpf <mirko@confluent.io>
2021-04-22 06:59:28 +0200

    prep clean graphdb/neo4j/data/transactions/system/neostore.transaction.db.0

--- bin/clean_git.sh
+++ bin/clean_git.sh
@@ -1,1 +1,4 @@
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch graphdb/neo4j/data/transactions/neo4j/neostore.transaction.db.0' --prune-empty --tag-name-filter cat -- --all
+cd ..
+git add .
+git commit -m "prep clean $1"
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch $1' --prune-empty --tag-name-filter cat -- --all
